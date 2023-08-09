f61d03e969da1d5d30338c92e0e60cb694134f9d
Joaquin Maroto Pascual <joaquinmarpas@gmail.com>
2023-08-09 19:27:53 +0200

    Changes

--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -4,15 +4,43 @@ const Task = require("../models/Task.model");
 const Category = require("../models/Category.model");
 
 // GET route ==>  Get all tasks within category
-router.get('/tasks', async (req, res) => {
+router.get('/tasks/:categoryId', async (req, res) => {
+    const { categoryId } = req.params;
     try {
-        let response = await Task.find()
-        return res.status(200).json(response);
-    } catch(err) {
+        // Find the category by its _id
+        const category = await Category.findById(categoryId);
+
+        if (!category) {
+            return res.status(404).json({ message: "Category not found" });
+        }
+
+        // Find tasks that have the matching category reference
+        const tasks = await Task.find({ category: categoryId });
+
+        return res.status(200).json(tasks);
+    } catch (err) {
         console.log(err);
-        res.status(500).json({ message: "Internal Server Error" })
+        res.status(500).json({ message: "Internal Server Error" });
     }
 });
+
+// GET route ==>  Get task by Id
+router.get('/tasks/:taskId', async (req, res) => {
+    const { taskId } = req.params;
+    try {
+        // Find the category by its _id
+        const task = await Task.findById(taskId);
+
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        return res.status(200).json(task);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
 
 // POST /api/tasks  -  Creates a new task
 router.post("/tasks/new", async (req, res) => {
@@ -48,15 +76,26 @@ router.put('/tasks/:taskId', async (req, res) => {
 // DELETE route ==>  Delete Task by Id
 router.delete('/tasks/:taskId', async (req, res) => {
     const { taskId } = req.params;
-   
     if (!mongoose.Types.ObjectId.isValid(taskId)) {
-      res.status(400).json({ message: 'Specified id is not valid' });
-      return;
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
     }
     try {
+        // Find the task to be deleted
+        const task = await Task.findById(taskId);
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        // Remove task reference from category
+        const category = await Category.findByIdAndUpdate(task.category, { $pull: { tasks: task._id } });
+        if (!category) {
+            return res.status(404).json({ message: "Category not found" });
+        }
+
+        // Delete the task
         await Task.findByIdAndRemove(taskId);
-        return res.status(204).json({message: `Category with ${taskId} is removed successfully.` });
-    } catch {
+        return res.status(204).json({ message: `Task with ID ${taskId} is removed successfully.` });
+    } catch (err) {
         console.log(err);
         return res.status(500).json({ message: "Internal Server Error" });
     }
